12ebe9e4432c3d05973917ad765e6606c7d75a9a
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-06-30 15:42:40 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,52 @@
 import Senators from './data/senators'
 
 export const republicans = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter (Senator => Senator.party === "Republican")
 }
 
 export const democrats = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter (Senator => Senator.party === "Democrat")
 }
 
 export const independents = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter (Senator => Senator.party === "Independent")
 }
 
 export const males = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter (Senator => Senator.person.gender_label === "Male")
 }
 
 export const females = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter (Senator => Senator.person.gender_label === "Female")
 }
 
 export const byState = (state = 'UT') => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter (Senator => Senator.state === state)
 }
 
 export const mapping = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.map (Senator => {
+    const senatorObj = {};
+      senatorObj.firstName = Senator.person.firstname;
+      senatorObj.lastName = Senator.person.lastname;
+      senatorObj.party = Senator.party;
+      senatorObj.gender = Senator.person.gender;
+  
+      return senatorObj;
+  });
 }
 
 export const reducedCount = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.reduce((tally, senator) => {
+    if (senator.party === "Republican"){
+       tally.republican++;
+    } else if (senator.party === "Democrat"){
+       tally.democrat++;
+    } else  {tally.independent++;}
+
+    return tally;
+  }, {republican: 0,democrat: 0,independent: 0});
 }
 
 
-const REPLACE_ME_WITH_CODE = false
+
